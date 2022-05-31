889e0e79e95243553ea373b0eaff7b3f2d602be3
Mohamed Mohamud <mohamedmohamud@Mohameds-MacBook-Pro.local>
2022-05-31 19:54:36 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -5,7 +5,7 @@ public class gitAndGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("today we learn github");
-		 
+		System.out.print("today is a very good day"); 
 	}
 
 }
